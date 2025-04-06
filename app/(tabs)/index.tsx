3283097502c9d0b8352ff9f7cedcd1e248cf276f
Vladyslav <vladbuldenko@gmail.com>
2025-04-06 18:13:47 +0300

    Handle refresh addition to home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, ScrollView, View } from "react-native";
+import { FlatList, RefreshControl, ScrollView, View } from "react-native";
 import { styles } from "../../styles/auth.styles";
 import Header from "@/components/layout/Header";
 import Stories from "@/components/Stories";
@@ -7,12 +7,21 @@ import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import Loader from "@/components/layout/Loader";
 import NoPostsFound from "@/components/NoPostsFound";
+import { useState } from "react";
+import { COLORS } from "@/constants/theme";
 
 export default function Index() {
+  const [refreshing, setRefreshing] = useState(false);
   const posts = useQuery(api.posts.getFeedPosts);
 
   if (!posts) return <Loader />;
   if (posts.length === 0) return <NoPostsFound />;
+
+  const handleRefresh = async () => {
+    setRefreshing(true);
+    // await posts.refetch();
+    setRefreshing(false);
+  };
 
   return (
     <View style={styles.container}>
@@ -25,6 +34,13 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 60 }}
         ListHeaderComponent={<Stories />}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+            tintColor={COLORS.primary}
+          />
+        }
       />
     </View>
   );
